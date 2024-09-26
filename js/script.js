a7620d2503976e22900b0b5cc9135c628221006d
gee_santosp <geovanna.gps.veleiros@gmail.com>
2024-09-26 16:26:46 -0300

    update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const toggleTheme = document.getElementById("toggleTheme");
 
 const rootHtml = document.documentElement;
 
-
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+const menuLinks = document.querySelectorAll(".menu-link");
 //função para alternar o tema 
 function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
@@ -17,3 +18,11 @@ function changeTheme () {
 toggleTheme.addEventListener("click", changeTheme);
 //fim da função mudar o tema
 
+accordionHeaders.forEach(header => {
+    header.addEventListener("click",() =>{
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
+    })
+})
